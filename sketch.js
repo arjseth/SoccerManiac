d084643e8d00096cdd35bc695435e7dc116b6df5
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-02 10:13:20 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let field;
+
+function setup() {
+  createCanvas(500, 300);
+  field = loadImage('penalty.jpeg');
+  img = loadImage('euro.png');
+}
+
+function draw() {
+  background(field);
+  image(img, 240, 240, 40, 40);
+  
+}
