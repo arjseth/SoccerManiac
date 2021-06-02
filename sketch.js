3913ed5f5b524ba23d8a8595301df6fedff0bd38
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-02 11:05:27 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,12 +3,16 @@ let field;
 
 function setup() {
   createCanvas(500, 300);
+  //This is where I give field an image
   field = loadImage('penalty.jpeg');
+  // This is where I give img my image
   img = loadImage('euro.png');
 }
 
 function draw() {
+  //This is where the background is being implmented
   background(field);
+  //This is where the ball is being implement
   image(img, 240, 240, 40, 40);
   
 }
