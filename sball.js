3066ebddca0448aeca38b185d972a207e4b2cc1b
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-03 09:59:57 -0400

    Update sball.js

--- sball.js
+++ sball.js
@@ -1,9 +1,10 @@
 // Making the ball with parameters an image, an x and y cordinate
 class Ball {
-  constructor(img, x, y) {
+  constructor(img, x, y, speed) {
     this.pic = img;
     this.x = x;
     this.y = y;
+    this.speed = speed;
   }
   
 }
