713c81104bb4d4f9c3d7e740249d8e8e2ea55f8a
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-08 10:00:48 -0400

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -1,5 +1,23 @@
-function arrow() {
-  fill(252, 3, 90);
-  rect(240, 240, 10, 20);
-  
+class Arrow {
+  constructor(x, y, x1, y1) {
+    this.x1 = 250;
+    this.y1 = 190;
+    this.pos = createVector(mouseX, mouseY);
+  }
+  update() {
+    this.pos.x = mouseX;
+    this.pos.y = mouseY;
+  }
+  show() {
+    fill(252, 3, 90);
+    triangle(
+      this.x1 + 30,
+      this.y1 + 50,
+      this.x1 - 10,
+      this.y1 + 50,
+      this.pos.x,
+      this.pos.y
+    );
+  }
 }
+
