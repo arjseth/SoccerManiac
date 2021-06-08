88e55cebda09b84266123e95675690d5c8d50b52
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-08 10:01:44 -0400

    Update sball.js

--- sball.js
+++ sball.js
@@ -1,10 +1,30 @@
 // Making the ball with parameters an image, an x and y cordinate
+var Bsize = 40;
 class Ball {
-  constructor(img, x, y, speed) {
+  constructor(img, x, y) {
     this.pic = img;
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    //copied from side scroller and edited for mine
+    this.pos=createVector(240, 240);
+    this.vel=createVector(0,0);
+    this.acc=createVector(0,0);
   }
   
+  update() {
+    //Copied from side scroller and changed for my program
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0,0);
+  }
+  show() { 
+    image(img, this.pos.x%400, this.pos.y%400, Bsize, Bsize);
+    
+  }
+  applyForce(f){
+    
+    f.sub(250,190);
+    f.div(10000);
+    //let v=createVector(1,--1);
+    this.acc.add(f);
+    Bsize -=0.05;
+  }
 }
