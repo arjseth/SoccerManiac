91a0abe92304f7fb72210b1aaa3393303ef96dbb
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-08 10:02:10 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-
 let field;
+let net = []
+let number;
+let Snum 
 
 function setup() {
   createCanvas(500, 300);
@@ -7,13 +9,39 @@ function setup() {
   field = loadImage('penalty.jpeg');
   // This is where I give img my image
   img = loadImage('euro.png');
+  Arrows = new Arrow();
+  Ball = new Ball();
+  number = floor(random(1, 5));
+  console.log(number)
+    
+    for (var i = 0; i < 5; i++) {
+    net.push(new Tar(185+i*30, 130+i*-10, color(250, 0, 0, 0), 30, number == i ))  
+  }
+}
+
+
+function game() {
+  //This is where the background is being implmented
+   background(field);
+  //This is where the ball is being implement
+  Arrows.show();
+  Arrows.update();
+  Ball.show();
+  Ball.update();
+  for (var i = 0; i < net.length; i++) {
+    net[i].update()
+    net[i].show()
+}
+ 
+  eval(net.length);
+  
+ 
+  
+  if (mouseIsPressed) {
+    Ball.applyForce(Arrows.pos);
+  }
 }
 
 function draw() {
-  //This is where the background is being implmented
-  background(field);
-  //This is where the ball is being implement
-  image(img, 240, 240, 40, 40);
-  arrow();
-  
+  game();
 }
