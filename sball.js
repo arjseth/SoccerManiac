39e9855dd86b62c2eee017a9d335619252bf7e09
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-09 11:12:35 -0400

    Update sball.js

--- sball.js
+++ sball.js
@@ -4,27 +4,39 @@ class Ball {
   constructor(img, x, y) {
     this.pic = img;
     //copied from side scroller and edited for mine
-    this.pos=createVector(240, 240);
-    this.vel=createVector(0,0);
-    this.acc=createVector(0,0);
+    this.pos = createVector(240, 240);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.score = 0;
   }
-  
+
   update() {
     //Copied from side scroller and changed for my program
     this.vel.add(this.acc);
     this.pos.add(this.vel);
-    this.acc.set(0,0);
+    this.acc.set(0, 0);
   }
-  show() { 
-    image(img, this.pos.x%400, this.pos.y%400, Bsize, Bsize);
-    
+  show() {
+    fill(255, 0, 0)
+    text("Score: " + this.score, 100, 100)
+    image(img, this.pos.x % 400, this.pos.y % 400, Bsize, Bsize);
   }
-  applyForce(f){
-    
-    f.sub(250,190);
+  applyForce(f) {
+    f.sub(250, 190);
     f.div(10000);
     //let v=createVector(1,--1);
     this.acc.add(f);
-    Bsize -=0.05;
+    Bsize -= 0.05;
+  }
+  collision(o, n) {
+    if (
+      o.x >= this.pos.x &&
+      o.x <= this.pos.x + 80 &&
+      o.y >= this.pos.y &&
+      o.y <= this.pos.y + 80
+    ) {
+         this.score++;
+  
+    }
   }
 }
