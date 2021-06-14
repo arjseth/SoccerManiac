9aeab862d3cc358baea0cf7c242b312c9e412dcb
arjseth <83041253+arjseth@users.noreply.github.com>
2021-06-14 19:56:42 -0400

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -1,14 +1,18 @@
+// This is the class which will display an arrow. This arrow will help the user shoot/point to a certain target.
 class Arrow {
   constructor(x, y, x1, y1) {
+    //these are all the parameters my Arrow Class will have
     this.x1 = 250;
     this.y1 = 190;
     this.pos = createVector(mouseX, mouseY);
   }
+  //With this update the position x and y will be controlled by the users mouseX and mouseY
   update() {
     this.pos.x = mouseX;
     this.pos.y = mouseY;
   }
   show() {
+    // This is the points and color of my arrow the this.x1 are the points which will stay stable and not move
     fill(252, 3, 90);
     triangle(
       this.x1 + 30,
@@ -20,5 +24,6 @@ class Arrow {
     );
   }
 }
+
 
 
